a927683ee1a9bda19650f0dea6f58594b3b6a1ff
Thiago Goncalves <162183868+thpgoncalves@users.noreply.github.com>
2024-06-22 10:36:22 -0300

    Add files via upload

---
+++ Github_Favs/README.md
@@ -0,0 +1,28 @@
+
+# Projeto Web 
+
+Projeto avançado com intuito de fazer integrações entre as tecnologias e explorar lógicas das linguagens exploradas nos módulos até então.
+
+Criação de lógicas com JS para as funcionalidades da aplicação.
+
+Utilização de conceitos de CleanCode para a organização da aplicação.
+
+Utilização dos conceitos de SPA para criar diferentes 'abas' na aplicação na web.
+
+Feito com JavaScript, HTML e CSS.
+
+# Web Project 
+
+Advanced project with the aim of integrating technologies and exploring the logic of the languages explored in the modules so far.
+
+Creation of JS logic for the application's functionalities.
+
+Using CleanCode concepts to organize the application.
+
+Using SPA concepts to create different 'tabs' in the web application.
+
+Made with JavaScript, HTML and CSS.
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://github.com/thpgoncalves)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/thiago-pereira-goncalves/)
+
+
---
+++ Github_Favs/js/favorites.js
@@ -0,0 +1,133 @@
+import { GithubUser } from "./githubUser.js"
+
+export class Favorites {
+  constructor(root) {
+    this.root = document.querySelector(root)
+    this.load() 
+  }
+  
+  load() {
+    console.log(localStorage.getItem('@githubFavorites:'))
+    this.entries = JSON.parse(localStorage.getItem('@githubFavorites:')) || []
+  }
+  
+  save() {
+    localStorage.setItem('@githubFavorites:', JSON.stringify(this.entries))
+  }
+
+  async add(username) {
+    try {
+      const userExists = this.entries.find(entry => entry.login.toLowerCase() === username.toLowerCase())
+      
+      if(userExists) {
+        throw new Error('Usuário já cadastrado!')
+      }    
+
+      const user = await GithubUser.search(username)
+      
+      if(user.login === undefined) {
+        throw new Error('Usuário não encontrado!')
+      }
+
+      const newEntries = [user, ...this.entries]
+    
+      this.entries = newEntries 
+      this.update()
+      this.save()
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+  
+  delete(user) {
+    const filteredEntries = this.entries
+      .filter((entry) => entry.login !== user.login)
+    
+    this.entries = filteredEntries
+    this.update()
+    this.save()
+  }
+}
+
+export class FavoritesView extends Favorites {
+  constructor(root) {
+    super(root)
+    
+    this.tbody = this.root.querySelector('table tbody')
+
+    this.update()
+    this.onadd()
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+
+      this.add(value)
+    }
+  }
+
+  update() {
+    this.removeAllTr()
+
+    this.entries.forEach( user =>{
+      const row = this.createRow()
+
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
+
+      row.querySelector('.user p').textContent = user.name
+
+      row.querySelector('.user span').textContent = user.login
+
+      row.querySelector('.repositories').textContent = user.public_repos
+
+      row.querySelector('.followers').textContent = user.followers
+
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+        if(isOk) {
+          this.delete(user)
+        }
+      }
+
+      this.tbody.append(row)
+    })
+  }
+
+  createRow() {
+    const tr = document.createElement('tr')
+    
+    tr.innerHTML = `
+    <td class="user">
+      <img src="https://github.com/ThPGoncalves.png" alt="">
+      <a href="https://github.com/ThPGoncalves" target="_blank">
+        <p>Thiago Gonçalves</p>
+        <span>ThPgoncalves</span>
+      </a>
+    </td>
+    <td class="repositories">
+      76
+    </td>
+    <td class="followers">
+      100
+    </td>
+    <td class="remove">
+      <button>&times;</button>
+    </td>
+    `
+
+    return tr
+  }
+
+  removeAllTr() {
+    this.tbody.querySelectorAll('tr')
+      .forEach((tr) => {
+        tr.remove()
+      })
+  }
+}
